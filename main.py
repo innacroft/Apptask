f0587b68cb62aac9fd57042e8c8c4e3b13ddac9c
=Ingrid Natalia R O <innarov@hotmail.com>
2020-03-31 18:31:44 -0500

    primer hola mundo con la IP

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from flask import Flask,request, make_response, redirect
+from flask import Flask, request, make_response, redirect
+app = Flask(__name__)
 
-app= Flask(__name__) #aca se pone el nombre del app
-
-@app.route():
+@app.route('/')
 def index():
   user_ip = request.remote_addr
-  response= make_response(redirect('/hello'))
-  response.set_cookie=('user_ip',user_ip)
+  response = make_response(redirect('/hello'))
+  response.set_cookie('user_ip', user_ip)
   return response
 
-
-
-
-@app.route('/') #la ruta donde se va a ejecutar la funcion
-def hello():
+@app.route('/hello')
+def hello_world():
+#creamos nueva variable de la ip que detectamos en el browser
   user_ip = request.cookies.get('user_ip')
-  return 'hello world Flask, tu ip es {}'.format(user_ip)
+
+  return'Hello, Word, tu IP es {}'.format(user_ip)
+  
+if __name__ == "__main__":
+    app.run(debug=True)
