9e817655cd1701aafddded7ffd8f84c5497d042a
=Ingrid Natalia R O <innarov@hotmail.com>
2020-03-31 23:23:29 -0500

    manejo archivos estaticos: imagenes y css

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
 app = Flask(__name__)
 todos=['Comprar cafe', 'Enviar solicitud de compra','Entregar video al productor']
+
+
+
 @app.route('/')
 def index():
   user_ip = request.remote_addr
