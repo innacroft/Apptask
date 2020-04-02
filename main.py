98b2646031ed086175c6ab6b665b8743a8f367aa
=Ingrid Natalia R O <innarov@hotmail.com>
2020-04-02 11:43:32 -0500

    forms en flask

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template,session
 from flask_bootstrap import Bootstrap 
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+
 #inicializacion de librerias
 bootstrap= Bootstrap(app)
 
+app.config['SECRET_KEY']='SUPER SECRETO'
+
 todos=['Comprar cafe', 'Enviar solicitud de compra','Entregar video al productor']
+
+class LoginForm(FlaskForm):
+  username=StringField('Nombre de usuario',validators=[DataRequired()])
+  password=PasswordField('Password',validators=[DataRequired()])
+  submit= SubmitField('Enviar')
+
+
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -18,17 +33,21 @@ def no_server(error):
 def index():
   user_ip = request.remote_addr
   response = make_response(redirect('/hello'))
-  response.set_cookie('user_ip', user_ip)
+  #response.set_cookie('user_ip', user_ip)
+  session['user_ip']=user_ip
   return response
 
 @app.route('/hello')
-def hello_world():
+def hello():
 #creamos nueva variable de la ip que detectamos en el browser
 
-  user_ip = request.cookies.get('user_ip')
+  #user_ip = request.cookies.get('user_ip')
+  user_ip= session.get('user_ip')
+  login_form= LoginForm()
   context={
     'user_ip':user_ip,
-    'todos':todos
+    'todos':todos,
+    'login_form':login_form
   }
 
   return render_template('hello.html', **context)
